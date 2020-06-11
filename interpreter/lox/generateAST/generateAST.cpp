3e33917ce7ddc33a538849fa5999a18b4a6c00d0
Eduard Mandy <mandyedi@gmail.com>
2020-06-11 18:44:16 +0200

    Finish Expr class generation (Implementing Syntax Trees 5.2)

--- interpreter/lox/generateAST/generateAST.cpp
+++ interpreter/lox/generateAST/generateAST.cpp
@@ -16,16 +16,24 @@ void defineType(std::ofstream& outputStream, const std::string& baseName, const 
 void split(const std::string& line, char delimiter, std::vector<std::string>& tokens);
 std::string trim(const std::string& line);
 
+class A
+{
+public:
+    A(int x) : X(x) {}
+
+    const int X;
+};
+
 int main(int argc, char** argv)
 {
     std::ofstream outputStream;
     outputStream.open("..\\lox\\Expr.h");
 
     defineAst(outputStream, "Expr", {
-        "Binary   : Expr left, Token operator, Expr right",
+        "Binary   : Expr left, Token op, Expr right",
         "Grouping : Expr expression",
         "Literal  : Object value",
-        "Unary    : Token operator, Expr right"
+        "Unary    : Token op, Expr right"
         });
 
     outputStream.close();
@@ -35,6 +43,8 @@ int main(int argc, char** argv)
 
 void defineAst(std::ofstream& outputStream, const std::string& baseName, const std::vector<std::string>& types)
 {
+    outputStream << "#ifndef EXPR_H\n#define EXPR_H\n";
+    outputStream << "#include \"Token.h\"\n\n";
     outputStream << "class " << baseName << "\n{\n";
     outputStream << "public:\n";
     outputStream << "    virtual ~" << baseName << "() {}\n";
@@ -48,12 +58,44 @@ void defineAst(std::ofstream& outputStream, const std::string& baseName, const s
         std::string fields = trim(tokens[1]);
         defineType(outputStream, baseName, className, fields);
     }
+
+    outputStream << "#endif EXPR_H // EXPR_H\n";
 }
 
 void defineType(std::ofstream& outputStream, const std::string& baseName, const std::string& className, const std::string& fieldList)
 {
     outputStream << "class " << className << " : public " << baseName << "\n{\n";
     outputStream << "public:\n";
+
+    // Constructor
+    outputStream << "    " << className << "( " << fieldList << " )\n";
+    // Store paramters
+    std::vector<std::string> fields;
+    split(fieldList, ',', fields);
+    for (unsigned int i = 0; i < fields.size(); i++)
+    {
+        std::vector<std::string> fieldItems;
+        split(trim(fields[i]), ' ', fieldItems);
+        std::string name = trim(fieldItems[1]);
+        std::string nameCap = name;
+        nameCap[0] = std::toupper(nameCap[0]);
+        outputStream << "    " << (i == 0 ? ": " : ", ") << nameCap << "(" << name << ")\n" ;
+    }
+    outputStream << "{}\n\n";
+
+    // Paramters
+    for ( auto& field : fields )
+    {
+        field = trim(field);
+        // Class parameters needs to be capitalzed
+        std::size_t pos = field.find(" ");
+        if (pos != std::string::npos && pos + 1 < field.size())
+        {
+            field[pos + 1] = std::toupper(field[pos + 1]);
+        }
+
+        outputStream << "    const " << field << ";\n";
+    }
 
     outputStream << "};\n\n";
 }
--- interpreter/lox/lox/Expr.h
+++ interpreter/lox/lox/Expr.h
@@ -1,3 +1,7 @@
+#ifndef EXPR_H
+#define EXPR_H
+#include "Token.h"
+
 class Expr
 {
 public:
@@ -7,20 +11,47 @@ public:
 class Binary : public Expr
 {
 public:
+    Binary( Expr left, Token op, Expr right )
+    : Left(left)
+    , Op(op)
+    , Right(right)
+{}
+
+    const Expr Left;
+    const Token Op;
+    const Expr Right;
 };
 
 class Grouping : public Expr
 {
 public:
+    Grouping( Expr expression )
+    : Expression(expression)
+{}
+
+    const Expr Expression;
 };
 
 class Literal : public Expr
 {
 public:
+    Literal( Object value )
+    : Value(value)
+{}
+
+    const Object Value;
 };
 
 class Unary : public Expr
 {
 public:
+    Unary( Token op, Expr right )
+    : Op(op)
+    , Right(right)
+{}
+
+    const Token Op;
+    const Expr Right;
 };
 
+#endif EXPR_H // EXPR_H
